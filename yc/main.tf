6d1933601dc16aafa39a312f0a9af1778148a490
BlackS52 <Black.S52@yandex.ru>
2022-05-11 15:04:27 -0400

    One comment added

--- yc/main.tf
+++ yc/main.tf
@@ -91,6 +91,7 @@ resource "yandex_compute_instance" "vm-1" {
   	}
 }
 
+# Just to user infrom
 output "internal_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.ip_address
 }
